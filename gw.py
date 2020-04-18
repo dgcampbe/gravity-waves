847b1437b3768c31ea6a2f5c7bfbc68edee6f86e
Dane Campbell <dgcampbe@gmail.com>
2020-04-18 00:36:54 -0400

    gw.py is now more object oriented, now has functions, and the random aproximation make a little more sense.
    Unfortunatly, not even simple gravity is accounted for still.

--- gw.py
+++ gw.py
@@ -15,48 +15,47 @@ class mass(object):
         self.mass = mass
         self.position = position
         self.momentum = momentum
+        self.df = pd.DataFrame({"mass":[self.mass],"position":[self.position],"momentum":[self.momentum]})
 
     def update(self, timestep):
         #Dane's super secret patent pending aproximation technique
-        import random
-        self.momentum = tuple([x+random.uniform(-1, 1) for x in self.momentum]) 
+        self.momentum = tuple([x*0.99 for x in self.momentum]) 
         self.position = tuple([y+ (x/self.mass*timestep) for (x,y) in zip(self.momentum, self.position)])
+        self.update_df()
+        
+    def update_df(self):
+        
+        self.df = self.df.append(pd.DataFrame({"mass":[self.mass], "position":[self.position], "momentum":[self.momentum]}), ignore_index=True)
 
-#Gather input from user
-print("For the below prompts, entering no value will enter default values.")
-time_init = eval("(" + input("Please enter inital conditions for time (timestep, totalsteps).") + ")")
-m1_init = eval("(" + input("Please enter intial conditions for mass1 (mass, position, momentum).") + ")")
-m2_init = eval("(" + input("Please enter intial conditions for mass2 (mass, position, momentum).") + ")")
+def prompt():
+    
+    #Gather input from user
+    print("For the below prompts, entering no value will enter default values.")
+    time_init = eval("(" + input("Please enter inital conditions for time (timestep, totalsteps).") + ")")
+    m1_init = eval("(" + input("Please enter intial conditions for mass1 (mass, position, momentum).") + ")")
+    m2_init = eval("(" + input("Please enter intial conditions for mass2 (mass, position, momentum).") + ")")
+    return [time_init,m1_init,m2_init]
 
-#default inits if no value provided
-if time_init == ():
+def empty_to_default(inits):
+    #default inits if no value provided
+    defaults = [(0.01,10000),(1,(1,1,1),(1,0,0)),(10,(0,0,0),(0,0,0))]
+    for i in range(len(inits)):
+        if inits[i] == ():
+            inits[i]=defaults[i]
+    return inits
 
-    time_init = (0.01,10000)
+def main():
+    [time_init,m1_init,m2_init] = empty_to_default(prompt())
+    #create masses
+    m1 = mass(*m1_init)
+    m2 = mass(*m2_init)
+    print("Aproximating for each time step. This may take a few seconds.")
+    for i in range(time_init[1]):
+        #update
+        m1.update(time_init[0])
+        m2.update(time_init[0])
+    #debugging
+    m1.df.to_csv("m1.csv",index=False)
+    m2.df.to_csv("m2.csv",index=False)
 
-if m1_init == ():
-
-    m1_init = (1,(1,1,1),(1,0,0))
-
-if m2_init == ():
-
-    m2_init = (10,(0,0,0),(0,0,0))
-
-#create pandas dataframes
-df1 = pd.DataFrame({"mass":[m1_init[0]],"position":[m1_init[1]],"momentum":[m1_init[2]]})
-df2 = pd.DataFrame({"mass":[m2_init[0]],"position":[m2_init[1]],"momentum":[m2_init[2]]})
-m1 = mass(*m1_init)
-m2 = mass(*m2_init)
-
-#loop over each time step
-for i in range(time_init[1]):
-    #update
-    m1.update(time_init[0])
-    m2.update(time_init[0])
-    #append new time step data to dataframes
-    df1 = df1.append(pd.DataFrame({"mass":[m1.mass], "position":[m1.position], "momentum":[m1.momentum]}), ignore_index=True)
-    df2 = df2.append(pd.DataFrame({"mass":[m2.mass], "position":[m2.position], "momentum":[m2.momentum]}), ignore_index=True)
-#debugging
-print(df1)
-print(df2)
-df1.to_csv("df1.csv",index=False)
-df2.to_csv("df2.csv",index=False)
+main()
