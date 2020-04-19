1552efc24cb2971e49c9305b6de3135c8bd92f74
Dane Campbell <dgcampbe@gmail.com>
2020-04-19 01:33:14 -0400

    Code is slightly cleaner.
    The code now uses numpy to simplify calculations and allows for inital conditions to be specificed through a configuration file. These should make the code easier to read and more straight forward.

--- gw.py
+++ gw.py
@@ -7,55 +7,49 @@ Created on Wed Apr 15 12:01:07 2020
 """
 import pandas as pd
 import matplotlib.pyplot as plt
+import numpy as np
 
 class mass(object):
     """Mass"""
-    def __init__(self, mass, position, momentum):
+    def __init__(self, init):
+        #Initialize mass
+        self.mass = init["mass"]
+        self.position = init["position"]
+        self.momentum = init["momentum"]
+        self.df = pd.DataFrame({"mass":[self.mass],"position":[self.position],"momentum":[self.momentum],"time":[0]})
 
-        self.mass = mass
-        self.position = position
-        self.momentum = momentum
-        self.df = pd.DataFrame({"mass":[self.mass],"position":[self.position],"momentum":[self.momentum]})
-
-    def update(self, timestep):
-        #Dane's super secret patent pending aproximation technique
-        self.momentum = tuple([x*0.99 for x in self.momentum]) 
-        self.position = tuple([y+ (x/self.mass*timestep) for (x,y) in zip(self.momentum, self.position)])
+    def update(self, time_init):
+        #Update position and momentum for a time step
+        self.momentum = np.multiply(self.momentum,0.99) 
+        self.position = np.add(self.position, np.multiply(self.momentum, time_init["time_step"]/self.mass))
         self.update_df()
         
     def update_df(self):
-        
+        #Add new row to df with current values
         self.df = self.df.append(pd.DataFrame({"mass":[self.mass], "position":[self.position], "momentum":[self.momentum]}), ignore_index=True)
 
-def prompt():
+def get_inits():
+    #Read initial conditions from inits.txt
+    inits = open("inits.txt","r")
+    time_init = eval(inits.readline())
+    m1_init = eval(inits.readline())
+    m2_init = eval(inits.readline())
+    return (time_init, m1_init, m2_init)
     
-    #Gather input from user
-    print("For the below prompts, entering no value will enter default values.")
-    time_init = eval("(" + input("Please enter inital conditions for time (timestep, totalsteps).") + ")")
-    m1_init = eval("(" + input("Please enter intial conditions for mass1 (mass, position, momentum).") + ")")
-    m2_init = eval("(" + input("Please enter intial conditions for mass2 (mass, position, momentum).") + ")")
-    return [time_init,m1_init,m2_init]
-
-def empty_to_default(inits):
-    #default inits if no value provided
-    defaults = [(0.01,10000),(1,(1,1,1),(1,0,0)),(10,(0,0,0),(0,0,0))]
-    for i in range(len(inits)):
-        if inits[i] == ():
-            inits[i]=defaults[i]
-    return inits
-
 def main():
-    [time_init,m1_init,m2_init] = empty_to_default(prompt())
-    #create masses
-    m1 = mass(*m1_init)
-    m2 = mass(*m2_init)
+    #Main
+    (time_init,m1_init,m2_init) = get_inits()
+    #Create masses
+    m1 = mass(m1_init)
+    m2 = mass(m2_init)
     print("Aproximating for each time step. This may take a few seconds.")
-    for i in range(time_init[1]):
-        #update
-        m1.update(time_init[0])
-        m2.update(time_init[0])
-    #debugging
+    for i in range(time_init["total_steps"]):
+        #Update
+        m1.update(time_init)
+        m2.update(time_init)
+    #Debugging
     m1.df.to_csv("m1.csv",index=False)
     m2.df.to_csv("m2.csv",index=False)
-
+    print("Data for masses has been exported to csv. Please check the files for results.")
+#Run main
 main()
